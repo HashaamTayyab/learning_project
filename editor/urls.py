584803c36c569b5c2e7e09227af5954004b1d0d5
asadiqbal-amalaxis <asad.iqbal@amalaxis.com>
2025-01-17 10:10:48 +0500

    Create urls.py

---
+++ editor/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import editor_dashboard, view_modules, view_subdots, view_topics, view_topic, subscribe_editor, add_topic, content_addition, add_subdot, login_view
+
+urlpatterns = [
+    path('', editor_dashboard, name='editor_dashboard'),
+    path('modules/<int:track_id>/', view_modules, name='view_modules'),
+    path('subdots/<int:module_id>/', view_subdots, name='view_subdots'),
+    path('topics/<int:subdot_id>/', view_topics, name='view_topics'),
+    path('topic/<int:topic_id>/', view_topic, name='view_topic'),
+    path('subscribe/', subscribe_editor, name='subscribe_editor'),
+    path('add_topic/<int:subdot_id>/', add_topic, name='add_topic'),  # New URL for adding topics
+    path('content_addition/', content_addition, name='content_addition'),  # New URL for content addition
+    path('add_subdot/<int:module_id>/', add_subdot, name='add_subdot'),  # New URL for adding subdots
+    path('login/', login_view, name='login'),  # New URL for login
+]
